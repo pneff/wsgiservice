37e31220fa1d8fcc8fa22ba6cbf229df7ab4aac3
Patrice Neff <www-github@patrice.ch>
2009-04-19 21:34:25 +0200

    Assert validations on a resource class.

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -6,3 +6,12 @@ def test_mount():
         pass
     assert Document.__name__ == 'Document'
     assert Document._path == '/{id}'
+
+def test_validate_resource():
+    @wsgiservice.validate('id', re=r'[-0-9a-zA-Z]{36}', doc='Document ID, must be a valid UUID.')
+    class User(wsgiservice.Resource):
+        pass
+    print User._validations
+    assert User.__name__ == 'User'
+    assert User._validations['id'] == {'re': r'[-0-9a-zA-Z]{36}',
+        'doc': 'Document ID, must be a valid UUID.'}

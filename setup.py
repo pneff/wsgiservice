883c7fd83d8b42dc31a0a34680c2f7a8a0ecc15f
Patrice Neff <www-github@patrice.ch>
2009-04-19 14:07:07 +0200

    Initial setup.py file with setuptools

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(name='WsgiService',
+      version='0.1',
+      description="A lean WSGI framework for easy creation of REST services",
+      author="Patrice Neff",
+      url='http://github.com/pneff/wsgiservice/tree/master',
+      packages=find_packages(),
+      install_requires=[
+      ],
+)

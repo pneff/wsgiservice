e17effffaff4efd23c76a721b498e5551f35bd9d
Patrice Neff <www-github@patrice.ch>
2009-05-02 21:16:27 +0200

    Better documentation for Resource methods. And move todo items to application in-code.

--- docs/todo.rst
+++ docs/todo.rst
@@ -1,16 +1,5 @@
 Todo
 ====
 
-.. todo:: Think about how to handle 201, 200/204 methods.
-.. todo:: Make downtime configurable with a file or something like that? Could then send out a 503 response with proper Retry-After header.
-.. todo:: Allow easy pluggin in of a compression WSGI middleware
-.. todo:: Convert to requested charset with Accept-Charset header
-.. todo:: Return Allow header as response to PUT and for 405 (also 501?)
-.. todo:: Implement Content-Location header
-.. todo:: Log From and Referer headers
-.. todo:: On 201 created provide Location header
-.. todo:: Abstract away error and status code handling
-.. todo:: Easy deployment using good configuration file handling
-.. todo:: Create usable REST API documentation from source
-.. todo:: Support OPTIONS, send out the Allow header, and some machine-readable output in the correct format. ``OPTIONS *`` can be discarded as NOOP
+.. todolist::
 
--- wsgiservice/__init__.py
+++ wsgiservice/__init__.py
@@ -15,4 +15,17 @@ duration = duration()
 
 class Resource(object):
     """Base class for all WsgiService resources. A resourse is a unique REST
-    endpoint which accepts different methods for different actions."""
+    endpoint which accepts different methods for different actions.
+    
+    For each HTTP call the corresponding method (equal to the HTTP method)
+    will be called. Additionally there are a few special methods used while
+    serving resources:
+    
+    * ``get_etag``: Returns a string to be used as the ETag for this resource.
+      Used to set the ``ETag`` response headers and for conditional requests
+      using the ``If-Match`` and ``If-None-Match`` request headers.
+    * ``get_last_modified``: Return a :class:`datetime.datetime` object of the
+      when the resource was last modified. Used to set the ``Last-Modified``
+      response header and for conditional requests using the
+      ``If-Modified-Since`` and ``If-Unmodified-Since`` request headers.
+    """

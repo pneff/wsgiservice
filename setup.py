4443f435ba7aecc0d6fa045bb2f42baae15ba09c
Patrice Neff <www-github@patrice.ch>
2009-06-23 16:34:31 +0200

    Prepare setup.py and __init__.py for release.

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 from setuptools import setup, find_packages
-setup(name='WsgiService',
-      version='0.1',
-      description="A lean WSGI framework for easy creation of REST services",
-      author="Patrice Neff",
-      url='http://github.com/pneff/wsgiservice/tree/master',
-      packages=find_packages(),
-      install_requires=[
-        'mox',
+print find_packages()
+setup(
+    name='WsgiService',
+    version='0.1',
+    description="A lean WSGI framework for easy creation of REST services",
+    author="Patrice Neff",
+    url='http://github.com/pneff/wsgiservice/tree/master',
+    packages=find_packages(),
+    install_requires=[
         'decorator',
         'webob',
-      ],
+    ],
+    tests_require=[
+        'nose',
+        'mox',
+    ],
+    test_suite='nose.collector',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ]
 )
--- wsgiservice/__init__.py
+++ wsgiservice/__init__.py
@@ -2,8 +2,13 @@
 made available here as well to keep the number of imports to a minimum for
 most applications.
 """
-import wsgiservice.routing
-from wsgiservice.decorators import mount, validate, expires
-from wsgiservice.application import get_app
-from wsgiservice.resource import Resource
-from wsgiservice.status import *
+__version__ = "0.1"
+__author__ = [
+    "Patrice Neff <software@patrice.ch>",
+]
+
+from decorators import mount, validate, expires
+from application import get_app
+from resource import Resource
+from status import *
+import routing

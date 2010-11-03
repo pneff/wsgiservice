8d7040394ef44b5972ed825240b85a4a864f5fc2
Patrice Neff <mail@patrice.ch>
2010-11-03 07:55:14 +0100

    Update to version 0.2.5

--- wsgiservice/__init__.py
+++ wsgiservice/__init__.py
@@ -2,7 +2,7 @@
 available here as well to keep the number of imports to a minimum for most
 applications.
 """
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __author__ = [
     "Patrice Neff <software@patrice.ch>",
 ]

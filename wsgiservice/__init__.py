1b923943bca1b4c73a2f6c8580a86aa557d5be39
jasonbrownbridge <jason@beekeeper.io>
2016-03-30 11:54:53 +0200

    Fix the version number in __init__.py

--- wsgiservice/__init__.py
+++ wsgiservice/__init__.py
@@ -2,7 +2,7 @@
 available here as well to keep the number of imports to a minimum for most
 applications.
 """
-__version__ = "0.4.0.dev2"
+__version__ = "0.4.3"
 
 from application import get_app
 from decorators import mount, validate, expires
